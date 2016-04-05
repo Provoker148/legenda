044d7d833979fbf0ec550c68e5f94cf4794c6eb0
Owl <owl.kz@yandex.ru>
2016-04-05 22:48:22 +0300

    Заменила надпись Очистить опции на "Очистить выбранные позиции"

--- catalog/language/russian/module/related_options.php
+++ catalog/language/russian/module/related_options.php
@@ -2,7 +2,7 @@
 // Related Options / Связанные опции
 // Support: support@liveopencart.com / Поддержка: help@liveopencart.ru
 
-$_['text_ro_clear_options'] = 'Очистить параметры';
+$_['text_ro_clear_options'] = 'Очистить выбранные позиции';
 $_['entry_stock_control_error']       = 'В корзину можно добавить только %s (для выбранного сочетания опций)';
 $_['ro_out_of_stock'] = 'Нет в наличии';
 
